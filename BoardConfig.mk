6ea16846a45592dd757f0eb1b71524cc986bce75
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-02-21 22:22:40 +0100

    msm8953-common: enable Wi-Fi Aware
    https://source.android.com/devices/tech/connect/wifi-aware
    
    Change-Id: I913f744eb4a41b1d84ac958b8efe06a4408e2182

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -224,6 +224,7 @@ BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
 WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+WIFI_HIDL_FEATURE_AWARE := true
 PRODUCT_VENDOR_MOVE_ENABLED := true
 
 # Inherit from the proprietary version
