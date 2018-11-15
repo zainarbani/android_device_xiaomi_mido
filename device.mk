fa5a922303f65fc93b3aa6b75851c2decbf476df
yourdeathwish <harangovindan@gmail.com>
2018-11-16 00:10:54 +0200

    msm8953-common: Build RCS
    * This is needed by IMS blobs and daemons for ims to work successfully

--- device.mk
+++ device.mk
@@ -305,6 +305,13 @@ PRODUCT_PACKAGES += \
     move_wifi_data.sh \
     ueventd.qcom.rc
 
+# RCS
+PRODUCT_PACKAGES += \
+    rcs_service_aidl \
+    rcs_service_aidl.xml \
+    rcs_service_api \
+    rcs_service_api.xml
+
 # RenderScript HAL
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
