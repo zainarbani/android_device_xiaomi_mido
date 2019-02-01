f07c4a1f798953048ba46eb3cafa89b9b8521b80
zainarbani <zaintsyariev@gmail.com>
2019-02-01 22:44:56 +0700

    mido: update camera prop
     -trebelized
     -enable HAL3
     -force HAL1 for some apps
    
    Signed-off-by: zainarbani <zaintsyariev@gmail.com>

--- prop.mk
+++ prop.mk
@@ -58,14 +58,17 @@ sys.vendor.shutdown.waittime=500
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-camera.display.lmax=1280x720 \
-camera.display.umax=1920x1080 \
-camera.lowpower.record.enable=1 \
+persist.vendor.camera.display.umax=1920x1080 \
+persist.vendor.camera.display.lmax=1280x720 \
+vendor.camera.lowpower.record.enable=1 \
 media.camera.ts.monotonic=1 \
 persist.camera.gyro.disable=0 \
 persist.camera.isp.clock.optmz=0 \
 persist.camera.stats.test=5 \
+persist.camera.HAL3.enabled=1 \
+persist.camera.eis.enable=1 \
 persist.vendor.qti.telephony.vt_cam_interface=1 \
+vendor.camera.hal1.packagelist=com.skype.raider,com.google.android.talk,com.whatsapp \
 vidc.enc.dcvs.extra-buff-count=2
 
 # Cne
